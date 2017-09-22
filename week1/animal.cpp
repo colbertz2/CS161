f745471df632b1ff3320d273f251f64f38c82c82
Zach Colbert <colbertz@oregonstate.edu>
2017-09-22 15:12:05 -0700

    Copied the content in from the assignment

--- week1/animal.cpp
+++ week1/animal.cpp
@@ -1,5 +1,20 @@
 /*********************************************************************
 ** Author: Zach Colbert
-** Date: 22 September 2017
-** Description: Assignment 1
+** Date: 27 September 2017
+** Description: A simple input/output program.
 *********************************************************************/
+
+#include <iostream>
+#include <string>
+
+// a simple example program
+int main()
+{
+  std::string faveAnimal;
+  std::cout << "Please enter your favorite animal." << std::endl;
+  std::cin >> faveAnimal;
+  std::cout << "Your favorite animal is the " << faveAnimal;
+  std::cout << "." << std::endl;
+
+  return 0;
+}
