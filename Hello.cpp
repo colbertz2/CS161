c0389c0a0d23ff2b95857213e6698050ab64c584
Zach Colbert <colbertz@oregonstate.edu>
2017-09-22 14:05:12 -0700

    Commented out namespace line Added std:: before each cout

--- Hello.cpp
+++ Hello.cpp
@@ -5,11 +5,11 @@
 *********************************************************************/
 
 #include <iostream>
-using namespace std;
+// using namespace std;
 
 int main()
 {
-  cout << "Hi! It\'s me.\n";
-  cout << "I\'m learning to program!\n";
+  std::cout << "Hi! It\'s me.\n";
+  std::cout << "I\'m learning to program!\n";
   return 0;
 }
