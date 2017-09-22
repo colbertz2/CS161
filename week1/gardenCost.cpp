1811f74f251b98ac03172fb5ea5b41942c1797cf
Zach Colbert <colbertz@oregonstate.edu>
2017-09-22 15:26:48 -0700

    Set up the bare structure of the source

---
+++ week1/gardenCost.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+** Author: Zach Colbert
+** Date: 27 September 2017
+** Description: A simple input/output program with mathematical operations. By prompting the user for three floats, the program will calculate and output a sum.
+*********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+// A simple input/output program with mathematical operations.
+int main()
+{
+  
+}
