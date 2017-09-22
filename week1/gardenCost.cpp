288daef898526f3ebde22cf0113b2e23ef6281bb
Zach Colbert <colbertz@oregonstate.edu>
2017-09-22 15:39:50 -0700

    Complete source, first draft

--- week1/gardenCost.cpp
+++ week1/gardenCost.cpp
@@ -11,21 +11,24 @@ using namespace std;
 int main()
 {
   // Declare variables for each of the input and output values
-
+  float soilCost, seedCost, fenceCost, totalCost;
 
   // Prompt the user, get the soil cost
-
+  cout << "What does the soil cost?\n";
+  cin >> soilCost;
 
   // Prompt the user, get the seed cost
-
+  cout << "What do the flower seeds cost?\n";
+  cin >> seedCost;
 
   // Prompt the user, get the fence cost
-
+  cout << "What does the fence cost?\n";
+  cin >> fenceCost;
 
   // Calculate the total cost of the three items
-
+  totalCost = soilCost + seedCost + fenceCost;
 
   // Display the total cost
-
-  
+  cout << "The total cost is " << totalCost << endl;
+  return 0;
 }
