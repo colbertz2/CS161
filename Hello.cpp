c151d579d808321e832e73f75d80c534cf824ac8
Zach Colbert <colbertz@oregonstate.edu>
2017-09-22 01:13:26 -0700

    Added Hello.cpp from tutorial in textbook, page 23.

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+/*********************************************************************
+** Author: Zach Colbert
+** Date: 22 September 2017
+** Description: This program prints a message with your name in it.
+*********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hi! It\'s me.\n";
+  cout << "I\'m learning to program!\n";
+  return 0;
+}
