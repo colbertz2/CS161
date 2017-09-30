35afc5e178a5dd3f2f6da3075ff56d6518abfc65
Zach Colbert <colbertz@oregonstate.edu>
2017-09-30 15:49:00 -0700

    Started building main function

--- week2/main.cpp
+++ week2/main.cpp
@@ -1,5 +1,29 @@
 /*********************************************************************
 ** Author: Zach Colbert
 ** Date: 4 October 2017
-** Description:
+** Description: Takes five floats as input, and returns their mean.
 *********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+// The function takes five floats as input from the user, and returns their mean.
+int main()
+{
+    // Declare variables for each of the inputs and the output
+    float input1, input2, input3, input4, input5, outputMean;
+
+    //Prompt the user for each of the necessary inputs
+    cout << "Please enter five numbers." << endl;
+    cin >> input1;
+    cin >> input2;
+    cin >> input3;
+    cin >> input4;
+    cin >> input5;
+
+    // Use another function to find the mean?
+
+    // Output the mean
+    // cout << "The average of those numbers is:\n";
+    // cout << outputMean;
+}
