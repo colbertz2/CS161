ff64eab14bd13720c79de300c833c3fa39e6252a
Zach Colbert <colbertz@oregonstate.edu>
2017-10-02 10:37:45 -0700

    Working version. Not formatted for submission yet.

--- week2/main.cpp
+++ week2/main.cpp
@@ -5,13 +5,18 @@
 *********************************************************************/
 
 #include <iostream>
-using namespace std;
+// using namespace std;
+    // TA said not to use namespace declaration like this
+using std::cout;
+using std::cin;
+using std::endl;
+    // Supplementary content said this method is best
 
 // The function takes five floats as input from the user, and returns their mean.
 int main()
 {
     // Declare variables for each of the inputs and the output
-    double input1, input2, input3, input4, input5, outputMean;
+    double input1, input2, input3, input4, input5, sumOfInputs, avgOfInputs;
 
     //Prompt the user for each of the necessary inputs
     cout << "Please enter five numbers." << endl;
@@ -21,10 +26,17 @@ int main()
     cin >> input4;
     cin >> input5;
 
-    // Use another function to find the mean?
-    // function avg()
+    // Calculate the mean of the input variables
+    sumOfInputs = input1 + input2 + input3 + input4 + input5;
+    avgOfInputs = sumOfInputs / 5;
+
+    // Some garbage to test my output variable
+    cout << "Sum of inputs: " << sumOfInputs << endl;
+    cout << "Average of inputs: " << avgOfInputs << endl;
 
     // Output the mean
     // cout << "The average of those numbers is:\n";
     // cout << outputMean;
+
+    return 0;
 }
