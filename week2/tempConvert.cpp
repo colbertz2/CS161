eab68fb257fb55df3bdf279a898ceb571e015615
Zach Colbert <colbertz@oregonstate.edu>
2017-10-02 18:36:58 -0700

    Working copy

--- week2/tempConvert.cpp
+++ week2/tempConvert.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
 ** Author: Zach Colbert
 ** Date: 4 October 2017
-** Description: Takes five floats as input, and returns their mean.
+** Description: This program converts a given temperature from degrees Celsius to degrees Fahrenheit.
 *********************************************************************/
 
 #include <iostream>
@@ -10,7 +10,21 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+// Converts a given temperature from degrees Celsius to degrees Fahrenheit
 int main()
 {
-    
+    // Declare variables for later
+    double tempF, tempC;
+
+    // Prompt the user for Celsius temperature to convert
+    cout << "Please enter a Celsius temperature." << endl;
+    cin >> tempC;
+
+    // Convert tempC (Celsius) to tempF (Fahrenheit)
+    tempF = (9 * tempC / 5) + 32;
+
+    // Print tempF for the user
+    cout << "The equivalent Fahrenheit temperature is:" << endl << tempF << endl;
+
+    return 0;
 }
