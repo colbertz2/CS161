dda4eb3705242b1d3416607839ed3cafcaf1f57a
Zach Colbert <colbertz@oregonstate.edu>
2017-10-02 18:22:47 -0700

    Started project 2.b

---
+++ week2/tempConvert.cpp
@@ -0,0 +1,16 @@
+/*********************************************************************
+** Author: Zach Colbert
+** Date: 4 October 2017
+** Description: Takes five floats as input, and returns their mean.
+*********************************************************************/
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    
+}
