d97500a8c5b39035c5b98a9161a4f28c3453fc6c
Zach Colbert <colbertz@oregonstate.edu>
2017-10-09 12:26:26 -0700

    Outlined with pseudo

--- week3/minmax.cpp
+++ week3/minmax.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
 ** Author: Zach Colbert
-** Date: 
-** Description: 
+** Date: 11 October 2017
+** Description: Takes a set of integers as input, and returns the minimum and maximum values.
 *********************************************************************/
 
 #include <iostream>
@@ -10,9 +10,24 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-// The main function
+// Gets a set of integers from the user, and outputs the minimum and maximum values
 int main()
 {
-    // Code goes here
+	// Get the number of integers from the user
+	//     Initialize that many variables to store the integers
+
+    // Get the integers from the user and store them
+
+    // If the number of integers entered is not equal to the number of integers requested
+	//     Tell the user they entered the wrong number of integers
+	//     Get choice from the user: Retry entering integers, or start over
+	//     If user chooses to start over
+	// 	    Start over
+	//     Else if user chooses to retry
+	// 	    Get the integers from the user
+    // Else
+	//     Find the minimum and maximum of the entered integers
+	//     Print those for the user
+
     return 0;
 }
