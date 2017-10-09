428779b46093df9610150a82bf500f8e8d4e5cf3
Zach Colbert <colbertz@oregonstate.edu>
2017-10-09 11:04:21 -0700

    Started week 3 assignments

---
+++ week3/minmax.cpp
@@ -0,0 +1,18 @@
+/*********************************************************************
+** Author: Zach Colbert
+** Date: 
+** Description: 
+*********************************************************************/
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+// The main function
+int main()
+{
+    // Code goes here
+    return 0;
+}
