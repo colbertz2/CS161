1841abf962e1693e8e9289c477ddb6b9b3745786
Zach Colbert <colbertz@oregonstate.edu>
2017-10-09 22:56:55 -0700

    Started week 3

---
+++ week3/minmax.cpp
@@ -0,0 +1,18 @@
+/*********************************************************************
+** Author: Zach Colbert
+** Date: 11 October 2017
+** Description: Gets a set of integers from the user, and returns the minimum and maximum values.
+*********************************************************************/
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+// The main function 
+int main()
+{
+    // Code goes here
+    return 0;
+}
