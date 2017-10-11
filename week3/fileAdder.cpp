d8fcfc42e6130b8c00744b9f2e337c833f1281fe
Zach Colbert <colbertz@oregonstate.edu>
2017-10-11 01:11:44 -0700

    Started 3.b

---
+++ week3/fileAdder.cpp
@@ -0,0 +1,18 @@
+/*********************************************************************
+** Author: Zach Colbert
+** Date: 11 October 2017
+** Description: Gets an input file from the user, adds a list of integers from the file, and outputs to another file.
+*********************************************************************/
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+// The main function
+int main()
+{
+    // Code goes here
+    return 0;
+}
