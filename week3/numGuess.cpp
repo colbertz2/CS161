3ea4e2ee281aefcc288ba2fc5a7b3e9fbf27352b
Zach Colbert <colbertz@oregonstate.edu>
2017-10-11 18:14:54 -0700

    Started 3.c

---
+++ week3/numGuess.cpp
@@ -0,0 +1,23 @@
+/*********************************************************************
+** Author: Zach Colbert
+** Date: 11 October 2017
+** Description: 
+*********************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+
+// The main function
+int main()
+{
+    // Code goes here
+    return 0;
+}
