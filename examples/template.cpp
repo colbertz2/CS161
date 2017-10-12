27345f91bb64f5c06c420ce08679e4f448ec8603
Zach Colbert <colbertz@oregonstate.edu>
2017-10-11 18:05:11 -0700

    Updated template file

--- examples/template.cpp
+++ examples/template.cpp
@@ -5,10 +5,15 @@
 *********************************************************************/
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
 
 // The main function
 int main()
