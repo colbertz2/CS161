5b9a0f9bb444e29fd6bcd0f434c4ec10c488ab8d
Zach Colbert <colbertz@oregonstate.edu>
2017-10-11 17:44:30 -0700

    Working copy

--- week3/fileAdder.cpp
+++ week3/fileAdder.cpp
@@ -5,14 +5,62 @@
 *********************************************************************/
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
 
 // The main function
 int main()
-{
-    // Code goes here
+{   
+    // Declare variables for later
+    int ifData, ofData;
+    
+    // Prompt the user for the input file
+    cout << "Please enter your filename." << endl;
+    
+    // Store the input file name
+    string fileName;
+    cin >> fileName;
+
+    // Set up the filestream object and try to open the file
+    ifstream inputFile(fileName);
+
+    // If that fails, give the user an error
+    if (!inputFile)
+    {
+        cout << "could not access file" << endl;
+    }
+    else if (inputFile)
+    {
+        // Set the data for the output file to zero
+        ofData = 0;
+
+        // Read until the end of the file
+        while (inputFile >> ifData)
+        {
+            // Add the data we just read to the running total for the output file
+            ofData += ifData;
+        }
+
+        // We're done with the input file, close it
+        inputFile.close();
+
+        // Output the total from the above loop to our output file
+        ofstream outputFile("sum.txt");
+        outputFile << ofData;
+
+        // We're done with the output file, close it
+        outputFile.close();
+
+        // Notify the user
+        cout << "result written to sum.txt" << endl;
+    }
+
     return 0;
 }
