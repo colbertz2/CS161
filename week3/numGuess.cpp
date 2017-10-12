09abe11cb443fa502cfd1b42a23e32f5e3be5657
Zach Colbert <colbertz@oregonstate.edu>
2017-10-11 18:56:30 -0700

    First draft.

--- week3/numGuess.cpp
+++ week3/numGuess.cpp
@@ -18,6 +18,45 @@ using std::string;
 // The main function
 int main()
 {
-    // Code goes here
+    // Prompt the user for an integer
+    cout << "Enter the number for the player to guess." << endl;
+    
+    // Store the integer from the user
+    int goal;
+    cin >> goal;
+
+    // Prompt for a guess
+    cout << "Enter your guess." << endl;
+
+    // Store the guess and set counter to 1
+    int guess, guessCount;
+    cin >> guess;
+    guessCount = 1;
+
+    // While guess is wrong
+    while (guess != goal)
+    {
+        // Give the user feedback on their guess
+        if (guess < goal)
+        {
+            // Too low, try again
+            cout << "Too low - try again." << endl;
+        }
+        else if (guess > goal)
+        {
+            // Too high, try again
+            cout << "Too high - try again." << endl;
+        }
+
+        // Store another guess
+        cin >> guess;
+
+        // Add to the counter
+        guessCount++;
+    }
+
+    // Once the guess is right, print the counter
+    cout << "You guess it in " << guessCount << " tries." << endl;
+
     return 0;
 }
