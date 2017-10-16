f8f11469284aa4f3fb520a36fc1ad149e34a20be
Zach Colbert <colbertz@oregonstate.edu>
2017-10-16 12:03:13 -0700

    Gave it some structure

--- week4/fallDistance.cpp
+++ week4/fallDistance.cpp
@@ -4,15 +4,22 @@
 ** Description: Defines a function to find the vertical displacement of a falling body in a given time.
 *********************************************************************/
 
-// Main function MUST BE REMOVED before submitting the assignment!
+/*********************************************************************
+Testing section MUST BE REMOVED OR COMMENTED BEFORE SUBMISSION
+*********************************************************************/
+double fallDistance(double time);
+
 int main()
 {
     // Don't write the actual code here! Use this to call the real function.
     return 0;
 }
+/*********************************************************************
+End testing section
+*********************************************************************/
 
 // Description
-double fallDistance()
+double fallDistance(double time)
 {
     // The actual code goes here.
 }
