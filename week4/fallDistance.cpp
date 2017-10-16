200cdde4df942b977a0b0716c9135a0ef697cdc7
Zach Colbert <colbertz@oregonstate.edu>
2017-10-16 11:48:45 -0700

    Started 4.a

---
+++ week4/fallDistance.cpp
@@ -0,0 +1,18 @@
+/*********************************************************************
+** Author: Zach Colbert
+** Date: 18 October 2017
+** Description: Defines a function to find the vertical displacement of a falling body in a given time.
+*********************************************************************/
+
+// Main function MUST BE REMOVED before submitting the assignment!
+int main()
+{
+    // Don't write the actual code here! Use this to call the real function.
+    return 0;
+}
+
+// Description
+double fallDistance()
+{
+    // The actual code goes here.
+}
