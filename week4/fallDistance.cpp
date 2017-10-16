19dab835993a8323de9e35537afde2f7878d64e2
Zach Colbert <colbertz@oregonstate.edu>
2017-10-16 12:28:36 -0700

    Working copy ready to be tested

--- week4/fallDistance.cpp
+++ week4/fallDistance.cpp
@@ -4,22 +4,45 @@
 ** Description: Defines a function to find the vertical displacement of a falling body in a given time.
 *********************************************************************/
 
+// For std::pow()
+#include <cmath>
+
 /*********************************************************************
 Testing section MUST BE REMOVED OR COMMENTED BEFORE SUBMISSION
 *********************************************************************/
+// For testing
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
 double fallDistance(double time);
 
 int main()
 {
-    // Don't write the actual code here! Use this to call the real function.
+    // Declare a variable for the test input
+    double testInput;
+    
+    // Get a time from the command line
+    cout << "Enter a time falling:" << endl;
+    cin >> testInput;
+
+    // Print the result of the fallDistance function
+    cout << "The displacement is " << fallDistance(testInput) << "m" << endl;
+
     return 0;
 }
 /*********************************************************************
 End testing section
 *********************************************************************/
 
-// Description
+// The function takes a time as its argument, and returns the vertical displacement of a body falling from rest over that time.
 double fallDistance(double time)
 {
-    // The actual code goes here.
+    // One-liner: Calculate the displacement
+    double displacement = 0.5*9.8*std::pow(time,2);
+
+    // Return the displacement
+    return displacement;
 }
