1ca542e82b4953971c4f8525676c7e7d5a727afe
Zach Colbert <colbertz@oregonstate.edu>
2017-10-17 21:56:52 -0700

    Working draft with test stuff

--- week4/smallSort.cpp
+++ week4/smallSort.cpp
@@ -34,8 +34,24 @@ void smallSort(int &argA, int &argB, int &argC);
 
 int main()
 {
-    // Testing and development code goes here
-    // Don't write anything important in this section because it's going away!
+    // Set up the variables to sort and print them
+    int a = 5;
+    int b = 17;
+    int c = 2;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+
+    // Pass them to the function
+    smallSort(a,b,c);
+
+    cout << "\n" << "Sorting..." << "\n" << endl;
+
+    // Print them out after sorting
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
 
     return 0;
 }
@@ -46,8 +62,24 @@ End Testing
 // Takes three integer variables by reference, and sorts them in ascending order
 void smallSort(int &argA, int &argB, int &argC)
 {
-    // Sort the values
-    
-
-    return void;
+    // While things aren't in order
+    while (argA > argB || argB > argC)
+    {
+        // If a is larger than b
+        if (argA > argB)
+        {
+            // Swap them
+            int temp = argA;
+            argA = argB;
+            argB = temp;
+        }
+        // If b is larger than c
+        else if (argB > argC)
+        {
+            // Swap them
+            int temp = argB;
+            argB = argC;
+            argC = temp;
+        }
+    }
 }
