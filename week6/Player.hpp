cc01a15f74670206fb71847151b505cd1cec9b0f
Zach Colbert <colbertz@oregonstate.edu>
2017-10-31 19:59:02 -0700

    Started week6

---
+++ week6/Player.hpp
@@ -0,0 +1,47 @@
+/********************************************************************* 
+** Author: Zach Colbert
+** Date: 1 November 2017
+** Description:
+*********************************************************************/ 
+
+#include <string>
+
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+
+class Player
+{
+private:
+    // Member variables are for player name and stats
+    std::string name;
+    int points, rebounds, assists;
+
+public:
+    // getName returns the player's name
+
+
+    // setPoints sets the value of the points stat
+
+
+    // getPoints returns the value of the points stat
+
+
+    // setRebounds sets the value of the rebounds stat
+
+
+    // getRebounds returns the value of the rebounds stat
+
+
+    // setAssists sets the value of the assists stat
+
+
+    // getAssists returns the value of the assists stat
+
+
+    // hasMorePointsThan takes another player object as input
+    // Function returns boolean TRUE if the value of the called player's points stat is greater than the value of the argument player's points stat
+
+};
+
+#endif
---
+++ week6/Team.hpp
@@ -0,0 +1,20 @@
+/********************************************************************* 
+** Author: Zach Colbert
+** Date: 1 November 2017
+** Description:
+*********************************************************************/ 
+
+#ifndef TEAM_HPP
+#define TEAM_HPP
+
+class Team
+{
+private:
+    // Stuff
+
+public:
+    // More stuff
+
+};
+
+#endif
