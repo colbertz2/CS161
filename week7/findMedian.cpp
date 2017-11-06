c99a3c6fda0306491550706470279ee1e467d8bf
Zach Colbert <colbertz@oregonstate.edu>
2017-11-06 11:42:50 -0800

    Started week7

---
+++ week7/findMedian.cpp
@@ -0,0 +1,17 @@
+/*********************************************************************
+** Author: Zach Colbert
+** Date: 8 November 2017
+** Description: 
+*********************************************************************/
+
+// Includes the built-in array sorting function
+#include <algorithm>
+
+
+// Start defining actual functions here
+int findMedian()
+{
+    // Actual code goes here.
+
+    return 0;
+}
---
+++ week7/testMain.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+
+// Function prototype
+int findMedian();
+
+int main()
+{
+    // Testing and development code goes here
+    // Don't write anything important in this section because it's going away!
+
+    return 0;
+}
