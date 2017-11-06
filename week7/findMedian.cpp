b029a6bf2bac9989d850cc4948a83020f33735d8
Zach Colbert <colbertz@oregonstate.edu>
2017-11-06 12:30:36 -0800

    First draft. Not sure if this is what they're looking for in terms of two different methods for odd and even lengths.

--- week7/findMedian.cpp
+++ week7/findMedian.cpp
@@ -1,17 +1,31 @@
 /*********************************************************************
 ** Author: Zach Colbert
 ** Date: 8 November 2017
-** Description: 
+** Description: findMedian takes an array and the size of the array as arguments, sorts the array, and returns the median(s) of the array.
 *********************************************************************/
 
-// Includes the built-in array sorting function
+// findMedian uses the built-in array sorting function
 #include <algorithm>
 
 
-// Start defining actual functions here
-int findMedian()
+int findMedian(int arrayArg[], int arraySize)
 {
-    // Actual code goes here.
+    // Sort the array
+    std::sort(arrayArg, arrayArg + arraySize);
 
-    return 0;
+    // For odd-numbered lengths, return the median
+    // According to i = (n-1)/2
+    int median = arrayArg[(arraySize - 1) / 2];
+
+    // For even-numbered lengths, find two medians
+    // i = (n-1)/2 and i2 = n/2 where i will be floored
+    // Then average the two values and return that average (which will also be floored)
+    if (arraySize % 2 == 0)
+    {
+        int median2 = arrayArg[arraySize / 2];
+
+        median = (median + median2) / 2;
+    }
+
+    return median;
 }
--- week7/testMain.cpp
+++ week7/testMain.cpp
@@ -10,12 +10,16 @@ using std::ofstream;
 using std::string;
 
 // Function prototype
-int findMedian();
+int findMedian(int array[], int arraySize);
 
 int main()
 {
     // Testing and development code goes here
     // Don't write anything important in this section because it's going away!
+
+    int testArray[] = {1,2,3,4,5,6};
+
+    cout << findMedian(testArray, 6) << endl;
 
     return 0;
 }
