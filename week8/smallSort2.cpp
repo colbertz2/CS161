09790964929d90036d15dab557f1cb05e277ed21
Zach Colbert <colbertz@oregonstate.edu>
2017-11-15 16:53:22 -0800

    Changed logic for the example smallSort function, added main for testing

--- week8/smallSort2.cpp
+++ week8/smallSort2.cpp
@@ -4,6 +4,12 @@
 ** Description: smallSort2 takes the adresses of three int variables, and sorts the ints at their respective addresses.
 *******************************************************************************/
 
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
 void smallSort2(int &a, int &b, int &c)
 {
     // Do the thing
@@ -12,19 +18,16 @@ void smallSort2(int &a, int &b, int &c)
 // Takes three integer variables by reference, and sorts them in ascending order
 void smallSort(int &argA, int &argB, int &argC)
 {
-    // While things aren't in order
-    while (argA > argB || argB > argC)
+    // If a is larger than b
+    while (argA > argB)
     {
-        // If a is larger than b
-        if (argA > argB)
-        {
-            // Swap them
-            int temp = argA;
-            argA = argB;
-            argB = temp;
-        }
+        // Swap them
+        int temp = argA;
+        argA = argB;
+        argB = temp;
+
         // If b is larger than c
-        else if (argB > argC)
+        while (argB > argC)
         {
             // Swap them
             int temp = argB;
@@ -32,4 +35,31 @@ void smallSort(int &argA, int &argB, int &argC)
             argC = temp;
         }
     }
+}
+
+int main()
+{
+    // Set up the variables to sort and print them
+    int a, b, c;
+
+    cout << "Enter three integers:" << endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+
+    // Pass them to the function
+    smallSort(a,b,c);
+
+    cout << "\n" << "Sorting..." << "\n" << endl;
+
+    // Print them out after sorting
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+
+    return 0;
 }
