ce335df1d29367ddf6549fdab7f174e024c8a8c7
Zach Colbert <colbertz@oregonstate.edu>
2017-11-22 19:54:33 -0800

    Testing

--- week9/testBoard.cpp
+++ week9/testBoard.cpp
@@ -5,13 +5,20 @@
 #include "Board.hpp"
 #include <iostream>
 
+using std::cin;
+using std::cout;
+using std::endl;
+
 int main()
 {
     // Initialize a new board
     Board::Board myBoard;
 
     // Print the board to make sure it's empty
-    myBoard.printBoard();
+    // myBoard.printBoard();
+
+    // Print the game state
+    cout << "\n" << myBoard.gameState() << endl;
 
     return 0;
 }
