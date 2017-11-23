4394c67ba71a1cdee57237db52c8550209d024c5
Zach Colbert <colbertz@oregonstate.edu>
2017-11-22 17:34:02 -0800

    Started a test main file

---
+++ week9/testBoard.cpp
@@ -0,0 +1,17 @@
+/*************************************************
+ *         TEST MAIN FOR THE BOARD CLASS         *
+ ************************************************/
+
+#include "Board.hpp"
+#include <iostream>
+
+int main()
+{
+    // Initialize a new board
+    Board::Board myBoard;
+
+    // Print the board to make sure it's empty
+    myBoard.printBoard();
+
+    return 0;
+}
