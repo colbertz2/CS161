58623004dc00e3465a374a5dd8e4460534eea43a
Zach Colbert <colbertz@oregonstate.edu>
2017-11-30 18:27:15 -0800

    First draft

--- week10/Library.cpp
+++ week10/Library.cpp
@@ -26,18 +26,20 @@ Library::Library()
 /*********************************************************************
 ** Description:
 *********************************************************************/
-void Library::addBook(Book*)
+void Library::addBook(Book* newBook)
 {
-
+    // vector::push_back will add Book* to the end of the vector
+    holdings.push_back(newBook);
 }
 
 
 /*********************************************************************
 ** Description:
 *********************************************************************/
-void Library::addPatron(Patron*)
+void Library::addPatron(Patron* newPatron)
 {
-
+    // vector::push_back will add Patron* to the end of the vector
+    members.push_back(newPatron);
 }
 
 
@@ -46,6 +48,49 @@ void Library::addPatron(Patron*)
 *********************************************************************/
 std::string Library::checkOutBook(std::string pID, std::string bID)
 {
+    // if the specified Book is not in the Library, return "book not found"
+
+    Book* chkBook = getBook(bID);
+
+    if (chkBook == nullptr)
+    {
+        return "book not found";
+    }
+
+    // if the specified Patron is not in the Library, return "patron not found"
+
+    Patron* chkPatron = getPatron(pID);
+
+    if(chkPatron == nullptr)
+    {
+        return "patron not found";
+    }
+
+    // if the specified Book is already checked out, return "book already checked out"
+    if ((*chkBook).getCheckedOutBy() != nullptr)
+    {
+        return "book already checked out";
+    }
+
+    // if the specified Book is on hold by another Patron, return "book on hold by other patron"
+    if ((*chkBook).getRequestedBy() != nullptr)
+    {
+        return "book on hold by other patron";
+    }
+
+    // otherwise update the Book's checkedOutBy, dateCheckedOut and Location; if the Book was on hold for this Patron, update requestedBy; update the Patron's checkedOutBooks; return "check out successful"
+    (*chkBook).setCheckedOutBy(chkPatron);
+    (*chkBook).setDateCheckedOut(currentDate);
+    (*chkBook).setLocation(CHECKED_OUT);
+
+    if ((*chkBook).getRequestedBy() == (*chkBook).getCheckedOutBy())
+    {
+        (*chkBook).setRequestedBy(nullptr);
+    }
+
+    (*chkPatron).addBook(chkBook);
+
+    return "check out successful";
 
 }
 
@@ -55,6 +100,37 @@ std::string Library::checkOutBook(std::string pID, std::string bID)
 *********************************************************************/
 std::string Library::returnBook(std::string bID)
 {
+    // if the specified Book is not in the Library, return "book not found"
+    Book* chkBook = getBook(bID);
+
+    if (chkBook == nullptr)
+    {
+        return "book not found";
+    }
+
+    // if the Book is not checked out, return "book already in library"
+    if ((*chkBook).getCheckedOutBy() == nullptr)
+    {
+        return "book already in library";
+    }
+
+    // update the Patron's checkedOutBooks; update the Book's location depending on whether another Patron has requested it; update the Book's checkedOutBy; return "return successful"
+    (*(*chkBook).getCheckedOutBy()).removeBook(chkBook);
+    
+    // If the book is not on hold, location goes to ON_SHELF
+    if ((*chkBook).getRequestedBy() == nullptr)
+    {
+        (*chkBook).setLocation(ON_SHELF);
+    }
+    // Otherwise, it is on hold and goes to ON_HOLD_SHELF
+    else
+    {
+        (*chkBook).setLocation(ON_HOLD_SHELF);
+    }
+
+    (*chkBook).setCheckedOutBy(nullptr);
+
+    return "return successful";
 
 }
 
@@ -64,6 +140,35 @@ std::string Library::returnBook(std::string bID)
 *********************************************************************/
 std::string Library::requestBook(std::string pID, std::string bID)
 {
+    // if the specified Book is not in the Library, return "book not found"
+    Book* chkBook = getBook(bID);
+
+    if (chkBook == nullptr)
+    {
+        return "book not found";
+    }
+
+    // if the specified Patron is not in the Library, return "patron not found"
+    Patron* chkPatron = getPatron(pID);
+
+    if (chkPatron == nullptr)
+    {
+        return "patron not found";
+    }
+
+    // if the specified Book is already requested, return "book already on hold"
+    if ((*chkBook).getRequestedBy() != nullptr)
+    {
+        return "book already on hold";
+    }
+
+    // update the Book's requestedBy; if the Book is on the shelf, update its location to on hold; return "request successful"
+    (*chkBook).setRequestedBy(chkPatron);
+    
+    if ((*chkBook).getLocation() == ON_SHELF)
+    {
+        (*chkBook).setLocation(ON_HOLD_SHELF);
+    }
 
 }
 
@@ -73,6 +178,24 @@ std::string Library::requestBook(std::string pID, std::string bID)
 *********************************************************************/
 std::string Library::payFine(std::string pID, double payment)
 {
+    // takes as a parameter the amount that is being paid (not the negative of that amount)
+    if (payment < 0)
+    {
+        return "invalid payment amount";
+    }
+
+    // if the specified Patron is not in the Library, return "patron not found"
+    Patron* chkPatron = getPatron(pID);
+
+    if (chkPatron == nullptr)
+    {
+        return "patron not found";
+    }
+
+    // use amendFine to update the Patron's fine; return "payment successful"
+    (*chkPatron).amendFine(payment);
+
+    return "payment successful";
 
 }
 
@@ -82,19 +205,59 @@ std::string Library::payFine(std::string pID, double payment)
 *********************************************************************/
 void Library::incrementCurrentDate()
 {
-
+    currentDate++;
 }
 
 
 // Description
 Patron* Library::getPatron(std::string pID)
 {
+    bool patronFound = false;
+    int patronIndex = -1;
 
+    for (Patron* chkPatron : members)
+    {
+        patronIndex++;
+        if ((*chkPatron).getIdNum() == pID)
+        {
+            patronFound = true;
+            break;
+        }
+    }
+
+    if (patronFound == false)
+    {
+        return nullptr;
+    }
+    else
+    {
+        return members[patronIndex];
+    }
 }
 
 
 // Description
 Book* Library::getBook(std::string bID)
 {
+    bool bookFound = false;
+    int bookIndex = -1;
 
+    for (Book* chkBook : holdings)
+    {
+        bookIndex++;
+        if ((*chkBook).getIdCode() == bID)
+        {
+            bookFound = true;
+            break;
+        }
+    }
+
+    if (bookFound == false)
+    {
+        return nullptr;
+    }
+    else
+    {
+        return holdings[bookIndex];
+    }
 }
